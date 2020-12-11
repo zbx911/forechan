99727f7b2cdcf717fb56a5b2301ba1b16356d6c7
Fly me to the moon <git@bigly.dog>
2020-12-11 13:44:46 -0800

    parse

--- test.py
+++ test.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python3
 
+from argparse import ArgumentParser, Namespace
 from os.path import dirname, join, realpath
 from unittest.loader import TestLoader
 from unittest.runner import TextTestRunner
 
+_base_ = dirname(realpath(__file__))
+_parent_ = dirname(_base_)
+_tests_ = join(_base_, "tests", "cases")
+
+
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument("-v", "--verbosity", action="count", default=1)
+    return parser.parse_args()
+
 
 def main() -> None:
-    _base_ = dirname(realpath(__file__))
-    _parent_ = dirname(_base_)
-    _tests_ = join(_base_, "tests", "cases")
-
+    args = parse_args()
     loader = TestLoader()
     suite = loader.discover(_tests_, top_level_dir=_parent_, pattern="*.py")
-    runner = TextTestRunner()
+    runner = TextTestRunner(verbosity=args.verbosity)
     runner.run(suite)
 
 
