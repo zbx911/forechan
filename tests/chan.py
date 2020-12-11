b5ae571b37772fe8bb4d85c4926e1b73f3e63dfc
Fly me to the moon <git@bigly.dog>
2020-12-11 00:35:07 -0800

    test loader

--- tests/chan.py
+++ tests/chan.py
@@ -6,6 +6,7 @@ from unittest.suite import TestSuite
 from ..forechan.chan import Chan
 from ._base import BASE_CASES, Channel, HasChannel
 from ._da import mixin_matrix
+from inspect import getmembers
 
 
 class BaseSetup:
@@ -14,10 +15,17 @@ class BaseSetup:
             self.ch: Channel = Chan()
 
 
+class Moo(IsolatedAsyncioTestCase):
+    async def test(self) -> None:
+        self.assertEqual(5, 34)
+
+
 def load_tests(
     loader: TestLoader, standard_tests: TestSuite, pattern: Optional[str]
 ) -> None:
-    tests = (cls() for cls in mixin_matrix((BaseSetup.SetupChan,), BASE_CASES))
-    suite = TestSuite()
-    suite.addTests(tests)
-    return suite
+    ret = TestSuite()
+    ret.addTest(standard_tests)
+    for cls in mixin_matrix((BaseSetup.SetupChan,), BASE_CASES):
+        suite = loader.loadTestsFromTestCase(cls)
+        ret.addTests(suite)
+    return ret
