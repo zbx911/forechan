ffd73ae48346add61e564d21876705923bc3de74
Fly me to the moon <git@bigly.dog>
2020-12-11 00:54:58 -0800

    lint

--- tests/_da.py
+++ tests/_da.py
@@ -1,26 +1,26 @@
 from itertools import product
-from typing import Callable, Iterator, Optional, Type
+from typing import Callable, Iterable, Iterator, Optional, Type
 from unittest.case import TestCase
 from unittest.loader import TestLoader
 from unittest.suite import TestSuite
 
 
-def mixin_matrix(*classes: Iterator[Type]) -> Iterator[Type]:
+def polyclass_matrix(*classes: Iterable[Type]) -> Iterator[Type]:
 
     for bcs in product(*classes):
 
-        class Mixin_Class(*bcs):
+        class PolyClass(*bcs):
             __qualname__ = f"| {' <|> '.join(bc.__qualname__ for bc in bcs)} |"
 
-        yield Mixin_Class
+        yield PolyClass
 
 
 def mk_loader(
-    *tests: TestCase,
+    *tests: Type[TestCase],
 ) -> Callable[[TestLoader, TestSuite, Optional[str]], TestSuite]:
     def load_tests(
         loader: TestLoader, standard_tests: TestSuite, pattern: Optional[str]
-    ) -> None:
+    ) -> TestSuite:
         ret = TestSuite()
         ret.addTest(standard_tests)
         for cls in tests:
--- tests/chan.py
+++ tests/chan.py
@@ -1,9 +1,8 @@
 from unittest import IsolatedAsyncioTestCase
 
-
 from ..forechan.chan import Chan
 from ._base import BASE_CASES, Channel, HasChannel
-from ._da import mixin_matrix, mk_loader
+from ._da import mk_loader, polyclass_matrix
 
 
 class BaseSetup:
@@ -17,4 +16,4 @@ class Moo(IsolatedAsyncioTestCase):
         self.assertEqual(5, 5)
 
 
-load_tests = mk_loader(*mixin_matrix((BaseSetup.SetupChan,), BASE_CASES))
+load_tests = mk_loader(*polyclass_matrix((BaseSetup.SetupChan,), BASE_CASES))
