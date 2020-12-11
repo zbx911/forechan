e168734e91050b15b90e3281a289fc4bac844c05
Fly me to the moon <git@bigly.dog>
2020-12-10 23:53:15 -0800

    matrix

--- tests/_base.py
+++ tests/_base.py
@@ -1,7 +1,19 @@
-from typing import Protocol
+from inspect import getmembers, isclass
+from itertools import product
+from typing import Iterator, Protocol, Type
 from unittest import IsolatedAsyncioTestCase
 
 from ..forechan.types import Channel
+
+
+def mixin_matrix(*classes: Iterator[Type]) -> Iterator[Type]:
+
+    for bcs in product(*classes):
+
+        class C(*bcs):
+            __qualname__ = " |> ".join(bc.__qualname__ for bc in bcs)
+
+        yield C
 
 
 class HasChannel(Protocol):
@@ -21,3 +33,9 @@ class BaseCases:
             # await self.ch.send(1)
             # iden = await self.ch.recv()
             self.assertEqual(1, 1)
+
+
+def every_basecase() -> Iterator[Type]:
+    for _, member in getmembers(BaseCases):
+        if isclass(member) and issubclass(member, IsolatedAsyncioTestCase):
+            yield member
