ec1133bf386224cd8e805dfa6f08980a04dc7a5d
Fly me to the moon <git@bigly.dog>
2020-12-12 21:47:03 -0800

    merge operations

--- forechan/operations.py
+++ forechan/operations.py
@@ -1,11 +1,15 @@
 from asyncio.tasks import create_task
-from typing import AsyncIterator, Callable, TypeVar
+from typing import Any, AsyncIterator, Callable, Tuple, TypeVar
 
 from .chan import chan
 from .types import Chan, ChanClosed
 
 T = TypeVar("T")
 U = TypeVar("U")
+
+
+async def select(ch: Chan[Any], *chs: Chan[Any]) -> AsyncIterator[Tuple[Chan, Any]]:
+    pass
 
 
 async def trans(
@@ -24,3 +28,21 @@ async def trans(
 
     create_task(cont())
     return out
+
+
+async def fan_in(ch: Chan[T], *chs: Chan[T]) -> Chan[T]:
+    out: Chan[T] = chan()
+
+    async def cont() -> None:
+        async with out:
+            async for _, item in select(ch, *chs):
+                try:
+                    await out.send(item)
+                except ChanClosed:
+                    break
+
+    create_task(cont())
+    return out
+
+
+# def fan_out(ch: Chan[T])
--- forechan/select.py
+++
@@ -1,7 +0,0 @@
-from typing import Any, AsyncIterator, Tuple
-
-from .types import Chan, ChanClosed
-
-
-async def select(chan: Chan[Any], *chans: Chan[Any]) -> AsyncIterator[Tuple[Chan, Any]]:
-    pass
