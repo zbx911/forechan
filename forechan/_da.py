4d579e8b067d36d3819ddbd0886a5a59e87440ea
Fly me to the moon <git@bigly.dog>
2020-12-17 05:28:43 -0800

    ok

--- forechan/_da.py
+++ forechan/_da.py
@@ -1,12 +1,14 @@
+from asyncio.coroutines import iscoroutine
 from asyncio.futures import Future
 from asyncio.tasks import FIRST_COMPLETED, create_task, wait
 from itertools import chain
-from typing import Awaitable, Set, Tuple, TypeVar
+from typing import Awaitable, Sequence, Tuple, TypeVar
 
 T = TypeVar("T")
 
 
-async def race(aw: Awaitable[T], *aws: Awaitable[T]) -> Tuple[T, Set[Future[T]]]:
-    futs = (create_task(aw) for aw in chain((aw,), aws))
+async def race(aw: Awaitable[T], *aws: Awaitable[T]) -> Tuple[T, Sequence[Future[T]]]:
+    futs = tuple(create_task(a) if iscoroutine(a) else a for a in chain((aw,), aws))
     done, pending = await wait(futs, return_when=FIRST_COMPLETED)
-    return done.pop().result(), pending
+    ret = done.pop().result()
+    return ret, tuple(chain(done, pending))
--- tests/cases/da.py
+++ tests/cases/da.py
@@ -9,7 +9,7 @@ from ..consts import SMOL_REP_FACTOR
 class Race(IsolatedAsyncioTestCase):
     async def test_1(self) -> None:
         for i in range(1, SMOL_REP_FACTOR + 1):
-            sleeps = islice(iter(lambda: sleep(0), None), i)
+            sleeps = islice(iter(lambda: sleep(0, i), None), i)
             ready, pending = await race(*sleeps)
-            self.assertEqual(ready, 0)
+            self.assertEqual(ready, i)
             self.assertEqual(len(pending), i - 1)
