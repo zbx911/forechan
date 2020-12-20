d9ed09449c070bf4c536c70a3c059cdebd8c8fff
Fly me to the moon <git@bigly.dog>
2020-12-19 17:36:13 -0800

    inc ver

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="forechan",
-    version="0.1.0",
+    version="0.1.1",
     description="Go style CSP for Python",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
