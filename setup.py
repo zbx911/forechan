0ea7bfa2d353382989c52240d625bcc7a4cd482e
Fly me to the moon <git@bigly.dog>
2020-12-19 17:19:20 -0800

    push

--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+
+from setuptools import setup
+
+
+def slurp(path: str) -> str:
+    with open(path) as fd:
+        return fd.read()
+
+
+setup(
+    name="forechan",
+    version="0.1.0",
+    description="Go style CSP for Python",
+    long_description=slurp("README.md"),
+    long_description_content_type="text/markdown",
+    author="ms-jpq",
+    author_email="github@bigly.dog",
+    url="https://github.com/ms-jpq/forechan",
+    packages=["forechan"],
+)
