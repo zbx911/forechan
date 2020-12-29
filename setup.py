614d2af5b10ae72546b58e30a488ab2f05ef0a5a
Fly me to the moon <git@bigly.dog>
2020-12-29 10:25:11 -0800

    py.typed

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 
+from pathlib import Path
+
 from setuptools import find_packages, setup
+
+packages = find_packages(exclude=("tests*",))
+package_data = {pkg: ("py.typed",) for pkg in packages}
+
 
 setup(
     name="forechan",
     version="0.1.3",
     description="Go style CSP for Python",
-    long_description=open("README.md").read(),
+    long_description=Path("README.md").read_text(),
     long_description_content_type="text/markdown",
     author="ms-jpq",
     author_email="github@bigly.dog",
     url="https://github.com/ms-jpq/forechan",
-    packages=find_packages(exclude=("tests*",)),
+    packages=packages,
+    package_data=package_data,
 )
