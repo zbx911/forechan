65b8d0f1c2d26f8a634abda70661c48ec39a3c59
i love my dog <50598611+ms-jpq@users.noreply.github.com>
2021-01-01 20:27:05 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ package_data = {pkg: ("py.typed",) for pkg in packages}
 
 setup(
     name="forechan",
-    version="0.1.4",
+    python_requires=">=3.8.0",
+    version="0.1.5",
     description="Go style CSP for Python",
     long_description=Path("README.md").read_text(),
     long_description_content_type="text/markdown",
