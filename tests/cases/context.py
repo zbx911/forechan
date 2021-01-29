4cfa4ff89558b197ee88910dc0a6a52633244fa1
Fly me to the moon <git@bigly.dog>
2021-01-29 04:58:40 -0800

    ttl

--- tests/cases/context.py
+++ tests/cases/context.py
@@ -2,7 +2,7 @@ from asyncio import sleep
 from math import inf
 from unittest import IsolatedAsyncioTestCase
 
-from ...forechan.context import ctx_with_timeout
+from ...forechan.context import ctx_with_parent, ctx_with_timeout
 
 
 class ContextTimeout(IsolatedAsyncioTestCase):
@@ -49,10 +49,22 @@ class ContextHeirarchy(IsolatedAsyncioTestCase):
     async def test_1(self) -> None:
         ttl = inf
         ctx1 = await ctx_with_timeout(ttl, val=None)
-        ctx2 = await ctx_with_timeout(ttl, val=None, parent=ctx1)
-        ctx3 = await ctx_with_timeout(ttl, val=None, parent=ctx2)
+        ctx2 = ctx_with_parent(None, parent=ctx1)
+        ctx3 = ctx_with_parent(None, parent=ctx2)
 
         ctx1.cancel()
         self.assertFalse(ctx1)
         self.assertFalse(ctx2)
         self.assertFalse(ctx3)
+
+        self.assertAlmostEqual(ctx1.ttl(), ctx2.ttl())
+        self.assertAlmostEqual(ctx2.ttl(), ctx3.ttl())
+
+    async def test_2(self) -> None:
+        ttl = 10
+        ctx1 = await ctx_with_timeout(ttl, val=None)
+        ctx2 = ctx_with_parent(None, parent=ctx1)
+        ctx3 = ctx_with_parent(None, parent=ctx2)
+
+        self.assertAlmostEqual(ctx1.ttl(), ctx2.ttl(), places=2)
+        self.assertAlmostEqual(ctx2.ttl(), ctx3.ttl(), places=2)
